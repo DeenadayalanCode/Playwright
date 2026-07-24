f4ab51ff4e7cde5b6a1adf872207fb70c4faaeb9
DeenadayalanCode <deena8688@gmail.com>
2026-07-24 18:01:28 +0530

    Add files via upload
    Home Assignment -18th July 2026

---
+++ Primitive-Datatypes.js
@@ -0,0 +1,12 @@
+//  **********  Home Assignment- Primitive Data Type   **********
+let firstName = "Deena";
+let companyName = "Testleaf";
+let mobileNumber = 9876543210;
+let isAutomation = true;
+let hasPlaywright;
+
+console.log("firstName:", firstName, "-", typeof firstName);
+console.log("companyName:", companyName, "-", typeof companyName);
+console.log("mobileNumber:", mobileNumber, "-", typeof mobileNumber);
+console.log("isAutomation:", isAutomation, "-", typeof isAutomation);
+console.log("hasPlaywright:", hasPlaywright, "-", typeof hasPlaywright);
